eecd57d25a1b59b94eac643f419b83050597c19e
muslima <muslima241105@gmail.com>
2023-11-20 17:22:11 +0500

    first commit

---
+++ bin/prepare.dart
@@ -0,0 +1,32 @@
+import 'package:prepare/prepare.dart' as prepare;
+
+String arrayToString(List a){
+  String s = "";
+  for(int i = 0; i < a.length; i++){
+    s += a[i];
+  }
+  return s;
+}
+
+String getString(String t,  ){}
+
+void smth(){
+  List a = ["ABC","BCD","DBCBA"];
+  String txt = arrayToString(a);
+  print(txt);
+  int indexBegin;
+  int lengthMax = 1;
+  for(int i = 0; i < txt.length; i++){
+
+    while(txt.length==i){
+
+    }
+
+  }
+
+}
+
+
+void main(List<String> arguments) {
+  smth();
+}
---
+++ test/prepare_test.dart
@@ -0,0 +1,8 @@
+import 'package:prepare/prepare.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
